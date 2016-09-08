443fcfd62bf14ef1e87021d3d29cd0de0c0503db
am411 <nilegend@gmail.com>
2016-09-08 14:30:00 -0400

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // Declare variable you:
+        // String, int, or double
+        String s;
+        s = "name"; //initialize
+        String s2 = "nickname";
+        System.out.println(s);
+        // Rules of Naming variables
+        // 1. if, else, void, public
+        // avoid all reserved words
+        // 2. Can't start with a number
+        // 1adfbjkahsb is not good
+        // 3. Special Characters !@$#%^&*(
+        // 4. Spaces are evil
+        // First Name x
+        // firstName first_name
+        // 5. Reuse variable names
+        // String vs int
+        // int vs double
+        int a = 45;
+        double b = 3;
+            double sum = a + b;
+
+            a = 2342342;
+        int E,d = 0;
+        int sometime;
+        int thissssssss = 0;
+        String something = "";
+        // String object pieces of it
+        //Strings have methods.
+
+    }
+}
